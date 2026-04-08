7315375c71ae7c69f3e889ac882b413c0ba02350
Thitipong-PP <thitipong.pgpt@gmail.com>
2026-04-08 13:51:08 +0700

    Initail config folder

---
+++ config/database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000, 
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    throw error; 
+  }
+};
+
+module.exports = connectDB;
