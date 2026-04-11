161bc27bb0f440bddd4a25d667c88452717ff118
Thitipong-PP <thitipong.pgpt@gmail.com>
2026-04-11 17:28:26 +0700

    Config booking route for dentist role

--- routes/bookings.js
+++ routes/bookings.js
@@ -17,12 +17,12 @@ const {protect, authorize} = require('../middleware/auth');
 
 // Path and method
 router.route('/')
-    .get(protect, authorize('admin','user'), getBookings) // Only admin can get all booking //user only get their own booking
+    .get(protect, authorize('admin','user','dentist'), getBookings) // admin get all, user get their own, dentist get their bookings
     .post(protect, authorize('user'), createBooking); // Only user can create booking
 
 router.route('/:id')
-    .get(protect, authorize('admin','user'), getBooking)
-    .put(protect, authorize('admin','user'), updateBooking)
-    .delete(protect, authorize('admin','user'), deleteBooking);
+    .get(protect, authorize('admin','user','dentist'), getBooking)
+    .put(protect, authorize('admin','user','dentist'), updateBooking) // dentist can edit their bookings
+    .delete(protect, authorize('admin','user','dentist'), deleteBooking);
 
 module.exports = router;
